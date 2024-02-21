283c02761e08bc3a82c6b8d276f25797e60b91ed
Aimal Khan <aimal4910@gmail.com>
2024-02-21 11:52:44 +0500

    Add files via upload

--- ex.js
+++ ex.js
@@ -10,13 +10,14 @@ function loginWithFacebook() {
   FB.login(function(response) {
     if (response.authResponse) {
      console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', {fields: "name, email, picture"}, function(response) {
+     FB.api("/me", "GET", {"fields": "name, email, gender, id"}, function(response) {
       document.querySelector("#name").innerHTML = "Name: " + response.name;
       document.querySelector("#email").innerHTML = "Email: " + response.email;
-      document.querySelector("#pic").innerHTML = '<img src="response.picture.data.url" alt="aimal" />';
+      document.querySelector("#gender").innerHTML = "Gender: " + response.gender;
+      document.querySelector("#id").innerHTML = "ID: " + response.id;
     });
    } else {
      console.log('User cancelled login or did not fully authorize.');
    }
- }, {scope: "public_profile, email"});
+ });
 }
